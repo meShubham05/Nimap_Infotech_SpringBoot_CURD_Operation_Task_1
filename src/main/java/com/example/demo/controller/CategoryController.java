04638a88457b70a58bab8fb063ce537c1d95cdf9
Shubham Gajanan Borkar <112811023+meShubham05@users.noreply.github.com>
2025-01-30 12:53:52 +0530

    "Task Completed Successfully..."

--- src/main/java/com/example/demo/controller/CategoryController.java
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -21,34 +21,47 @@ public class CategoryController {
     public List<Category> getAllCategories()
     {
         return categoryService.getAllCategories();
+
+        // this method is not mentioned but have created for fetching the Categories
+        // fetching all Categories  using the request http://localhost:8080/api/categories/all
     }
 
 
     @GetMapping()
     public Page<Category> getAllCategories(@RequestParam(defaultValue = "0") int page) {
         return categoryService.getAllCategories(page);
+
+        // for fetching the Categories
     }
 
     @PostMapping
     public Category createCategory(@RequestBody Category category)
     {
         return categoryService.createCategory(category);
+
+        // for creating a new Category
     }
 
     @GetMapping("/{id}")
     public Optional<Category> getCategoryById(@PathVariable Long id)
     {
         return categoryService.getCategoryById(id);
+
+        // for fetching a Category based on the given category id
     }
 
     @PutMapping("/{id}")
     public Category updateCategory(@PathVariable Long id, @RequestBody Category categoryDetails) {
         return categoryService.updateCategory(id, categoryDetails);
+
+        // for updating the Category based on the category id
     }
 
     @DeleteMapping("/{id}")
     public void deleteCategory(@PathVariable Long id)
     {
         categoryService.deleteCategory(id);
+
+        // for deleting the specific category based on the category id
     }
 }
